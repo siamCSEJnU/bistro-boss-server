b0547d61cf4554883b85182b98c0f570545fe506
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-30 09:09:04 +0600

    api created to update an user into admin

--- index.js
+++ index.js
@@ -32,9 +32,34 @@ async function run() {
 
     //user related apis
 
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      // console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: "user already exist" });
+      }
+
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (rew, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
