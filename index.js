eb5d7cb8520c2dd62532b478b4de4ed7627859bb
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-30 06:57:25 +0600

    to store user credential users api created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || "5000";
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2jfyhqy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,14 +26,26 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("BistroDB").collection("menu");
+    const usersCollection = client.db("BistroDB").collection("users");
     const reviewsCollection = client.db("BistroDB").collection("reviews");
     const cartCollection = client.db("BistroDB").collection("carts");
 
+    //user related apis
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //menu related apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       //   console.log("hit");
       res.send(result);
     });
+
+    //review rellated apis
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       //   console.log("hit");
@@ -44,8 +56,6 @@ async function run() {
 
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      console.log(email);
-      console.log("email");
       if (!email) {
         res.send([]);
       }
@@ -58,6 +68,13 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
