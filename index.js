13f5c78a715a214fe9127678c45803e5b1e23ed3
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-06-03 05:00:57 +0600

    specific item deletion in manage items route created

--- index.js
+++ index.js
@@ -128,6 +128,16 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/menu/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      // const query = { _id: id };
+      const query = {
+        $or: [{ _id: id }, { _id: new ObjectId(id) }],
+      };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+
     //review rellated apis
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
